99f2acb212198b6cd83b9b755542be690152e770
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-28 11:34:21 +0100

    #

---
+++ Ejercicio_11.py
@@ -0,0 +1,7 @@
+# Escribir un programa que almacene las matrices:
+#    1 2 3       -1 0
+# A= 4 5 6    B=  0 1
+#                 1 1
+# en una lista y muestre por pantalla su producto.
+# Nota: Para representar matrices mediante listas usar listas anidadas,
+# representando cada vector fila en una lista.
